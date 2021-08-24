bc1cdce6bc97e308e60f8e35fa77b2ff03508f65
GDivino <68019100+GDivino@users.noreply.github.com>
2021-08-24 23:48:20 +0800

    Add events model

--- pavilion/orgs/models.py
+++ pavilion/orgs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Events(models.Model):
+    status_choices = [
+        ("Published", "Published"),
+        ("Draft", "Draft")
+    ]
+
+    name = models.CharField(max_length=100)
+    event_date = models.DateField(auto_now=False, auto_now_add=False)
+    last_updated = models.DateField(auto_now=True, auto_now_add=False)
+    status = models.CharField(max_length=50, choices=status_choices)
+    
