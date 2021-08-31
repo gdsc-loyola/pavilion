37a04599a439df877af3642c9300f8b01a386b2e
Znarfois <franztaborlupa.tech@gmail.com>
2021-08-31 21:37:58 +0800

    Create SignUp form

--- pavilion-web/src/org/components/SelfSignUp/SignUp.js
+++ pavilion-web/src/org/components/SelfSignUp/SignUp.js
@@ -7,7 +7,7 @@ import PrimaryButton from '../PrimaryButton.js'
 
 const SignUpForm = () => {
     return (
-        <div class="welcome-form">
+        <div className="welcome-form">
             <h1>Welcome to the Pavilion.</h1>
             <form method="POST" target="">
                 <input type="text" name="email" placeholder="Organization email" />
