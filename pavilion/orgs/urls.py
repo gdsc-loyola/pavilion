cdf7ae78b5ed1d621370afb6b60730db1ec5ce56
GDivino <68019100+GDivino@users.noreply.github.com>
2021-08-31 23:33:31 +0800

    Add org model to api

--- pavilion/orgs/urls.py
+++ pavilion/orgs/urls.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
 from django.urls import path, include
-from .api import EventsViewSet
+from .api import EventsViewSet, OrgsViewSet
 
 router = routers.DefaultRouter()
-router.register(r"orgs", EventsViewSet, "orgs")
+router.register(r"orgs", OrgsViewSet, "orgs")
+router.register(r"events", EventsViewSet, "events")
 
 urlpatterns = [
     path("api/", include(router.urls))
