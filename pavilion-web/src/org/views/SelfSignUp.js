7f74de6f0ab5f198991adc3f1702cbe476e25e5c
Znarfois <franztaborlupa.tech@gmail.com>
2021-08-31 20:05:54 +0800

    Create SSU Org Side View

---
+++ pavilion-web/src/org/views/SelfSignUp.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+import WelcomeForm from '../components/WelcomeForm'
+import "../../stylesheets/org/SelfSignUp.scss"
+
+const SelfSignUp = () => {
+    return (
+        <div className="self-sign-up">
+            <WelcomeForm />
+        </div>
+    )
+}
+
+export default SelfSignUp
