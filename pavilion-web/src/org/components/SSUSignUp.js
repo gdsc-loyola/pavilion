debf8b15966d1e9f52ca14506239f982e0d92c0c
Znarfois <franztaborlupa.tech@gmail.com>
2021-08-31 20:11:38 +0800

    Rename Welcome to SignUp and minor changes

--- pavilion-web/src/org/components/SSUSignUp.js
+++ pavilion-web/src/org/components/SSUSignUp.js
@@ -3,9 +3,9 @@ import React from 'react'
 
 // import stylesheets
 import "../../stylesheets/org/SecondaryButton.scss"
-import PrimaryButton from './PrimaryButton'
+import PrimaryButton from './PrimaryButton.js'
 
-const WelcomeForm = () => {
+const SignUpForm = () => {
     return (
         <div class="welcome-form">
             <h1>Welcome to the Pavilion.</h1>
@@ -16,11 +16,12 @@ const WelcomeForm = () => {
                 <input type="checkbox" name="auth_org_rep" />
                 <span>I am an authorized representative of this organization.</span>
                 {/* <input type="submit"><PrimaryButton button_copy="Create an account"/> */}
-                <p>Please fill out the fields to create an account.</p>
+                <p className="error_msg">Please fill out the fields to create an account.</p>
+                <PrimaryButton button_copy="Create an account"/>
             </form>
         </div>
     )
 }
 
-export default WelcomeForm;
+export default SignUpForm;
 
