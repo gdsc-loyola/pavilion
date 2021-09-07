b3b52483341cb7f3a5ccada465ab4ff906d5bb94
GDivino <68019100+GDivino@users.noreply.github.com>
2021-09-07 13:49:57 +0800

    comment out unnecessasary text

--- pavilion/pavilion/settings.py
+++ pavilion/pavilion/settings.py
@@ -142,7 +142,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-Run this only during production
+# Run this only during production
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
