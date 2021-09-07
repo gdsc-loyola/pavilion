2254f1ac6b9278a7b78b8485c81f6447bd21ba4f
GDivino <68019100+GDivino@users.noreply.github.com>
2021-09-07 14:20:25 +0800

    add frontend to staticfilesdir

--- pavilion/settings.py
+++ pavilion/settings.py
@@ -129,6 +129,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'static/frontend')
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
