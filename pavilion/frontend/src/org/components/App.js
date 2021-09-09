d5c5741dc995c545c2cf9d3377737a8048ffbc37
Znarfois <franztaborlupa.tech@gmail.com>
2021-09-09 15:42:28 +0800

    Change App.js to start in SSUOrgSide

--- pavilion/frontend/src/org/components/App.js
+++ pavilion/frontend/src/org/components/App.js
@@ -1,4 +1,5 @@
 import Dashboard from '../views/Dashboard';
+import SSUOrgSide from '../views/SelfSignUp';
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
@@ -6,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Dashboard />
+        <SSUOrgSide />
       </div>
     );
   }
