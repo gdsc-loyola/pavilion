3601a940376b0f0d419c85bcd3d40a5ca2e78059
GDivino <68019100+GDivino@users.noreply.github.com>
2021-09-21 23:55:14 +0800

    add comma for REST_FRAMEWORK settings

--- pavilion/settings.py
+++ pavilion/settings.py
@@ -166,7 +166,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
      'DEFAULT_RENDERER_CLASSES': (
          'rest_framework.renderers.JSONRenderer',
-    )
+    ),
     'DEFAULT_PERMISSION_CLASSES' : ('rest_framework.permissions.IsAuthenticated'),
     'DEFAULT_AUTHENTICATION_CLASSES' : ('rest_framework_simplejwt.authentication.JWTAuthentication',),
 }
