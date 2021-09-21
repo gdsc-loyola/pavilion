1e65e0282aadfc9eb65e3134e96e99c2f35637d6
GDivino <68019100+GDivino@users.noreply.github.com>
2021-09-22 00:41:07 +0800

    remove additional import

--- pavilion/settings.py
+++ pavilion/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
 import os
 from dotenv import load_dotenv
 import dj_database_url
