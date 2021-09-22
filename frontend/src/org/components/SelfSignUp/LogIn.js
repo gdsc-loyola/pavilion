30225f814da3857bbd7b583a731bd999ef9f7e16
GDivino <68019100+GDivino@users.noreply.github.com>
2021-09-22 19:49:17 +0800

    implement flow routing and data passing

--- frontend/src/org/components/SelfSignUp/LogIn.js
+++ frontend/src/org/components/SelfSignUp/LogIn.js
@@ -1,11 +1,49 @@
-// import essential dependencies
-import React from 'react'
-import TextField  from '@material-ui/core/Textfield'
-// import stylesheets
-// import "../../stylesheets/org/SecondaryButton.scss"
-import PrimaryButton from '../PrimaryButton.js'
+import React, {useEffect} from 'react'
+import Cotter from 'cotter';
+import auth from '../../authentication/auth';
+import authService from '../../../services/auth.service'
+import "../../../../stylesheets/org/SelfSignUp.scss"
+import axios from 'axios';
 
-const LogInForm = () => {
+const Login = props => {
+    useEffect(() => {
+        const cotter = new Cotter('aa2398ab-3950-42dd-b3d1-4e383734a5ac')
+        cotter
+          .withFormID("form_default") // Use customization for form "form_default"
+          .signInWithLink() // use .signInWithOTP() to send an OTP
+          .showEmailForm()  // use .showPhoneForm() to send magic link to a phone number 
+          .then(async (res) => {
+            let registerBackend
+            let loginBackend
+
+            const isRegistered = await authService.get(res.email)
+
+            // Signup
+            if (isRegistered.data === "signup") {
+                registerBackend = await authService.register(res.email)
+                loginBackend = await authService.login(registerBackend.data.username)
+                await auth.saveToken(loginBackend.data.access)
+                
+                // Redirect to signup
+                auth.signup(() => {
+                    props.history.push("/org-info/")
+                })
+
+            // Login
+            } else {
+                loginBackend = await authService.login(isRegistered.data.username)
+                await auth.saveToken(loginBackend.data.access)
+
+                // Redirect to login
+                auth.login(() => {
+                    axios.defaults.headers.common['Authorization'] = auth.getToken()
+                    props.history.push("/admin/")
+                })
+            }
+        })
+        .catch(err => console.log(err))
+    }, [])
+
     return (
         <div className="form-container login">
             <div className="blue-bg"></div>
@@ -26,12 +64,11 @@ const LogInForm = () => {
                         handleOrgNameChange
                     }}
                 /> */}
-                <div id="cotter-form-container" style={{ width: 300, height: 300 }} />
+                <div id="cotter-form-container" style={{ width: 300, height: 150 }} />
                 {/* <PrimaryButton button_copy="Log In"/> */}
             </div>
         </div>
     )
 }
 
-export default LogInForm;
-
+export default Login
--- frontend/urls.py
+++ frontend/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('admin/', views.index),
     path('admin/events/', views.index),
-    path('admin/login/', views.index)
+    path('admin/login/', views.index),
+    path('org-info/', views.index),
+    path('org-logo/', views.index),
+    path('org-links/', views.index)
 ]
