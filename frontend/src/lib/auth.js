9cdcf3b00b4718bc889d76d043ec67bec82a05b9
Paolo Tiu <paolotiu17@gmail.com>
2021-11-14 21:35:55 +0800

    Login page

---
+++ frontend/src/lib/auth.js
@@ -0,0 +1,76 @@
+import http from "./http";
+
+const IS_AUTH = "isAuth";
+const TOKEN_KEY = "auth-token";
+
+/**
+ * Login
+ *
+ * @param {Object} params
+ * @param {string} params.email
+ * @param {string} params.password
+ *
+ * @example login({email: "email@email.com", password: "Password123"})
+ *
+ */
+export const login = async ({
+  email,
+  password = "9,2Nli1H:C&Vmyl<9:Y)VV1t[jQN7rS7Laf|sip*]X_Fi)IX5",
+}) => {
+  const res = await http.post("login/", { username: email, password });
+  window.sessionStorage.removeItem(IS_AUTH);
+  window.sessionStorage.setItem(IS_AUTH, true);
+
+  window.sessionStorage.removeItem(TOKEN_KEY);
+  window.sessionStorage.setItem(TOKEN_KEY, token);
+  return res;
+};
+
+/**
+ * Register
+ *
+ * @param {Object} params
+ * @param {string} params.email
+ *
+ * @example register({email: "gdsc@gmail.com"})
+ */
+export const register = ({ email }) => {
+  return http.post("register/", { username: email });
+};
+
+/**
+ * Logout
+ */
+export const logout = async () => {
+  await http.post("logout/");
+  window.sessionStorage.setItem(IS_AUTH, false);
+};
+
+/**
+ * @param {Object} params
+ * @param {string} params.email
+ *
+ * @description Returns the user data or "signup" if the user doesn't exist
+ * @example getUser({emaiL: "gdsc@ateneo.com"})
+ */
+export const getUser = ({ email }) => {
+  return http.get(`users/?user=${email}`);
+};
+
+export const signup = () => {
+  window.sessionStorage.removeItem(IS_AUTH);
+  window.sessionStorage.setItem(IS_AUTH, true);
+};
+
+export const isAuthenticate = () => {
+  return window.sessionStorage.getItem(IS_AUTH);
+};
+
+export const saveToken = (token) => {
+  window.sessionStorage.removeItem(TOKEN_KEY);
+  window.sessionStorage.setItem(TOKEN_KEY, token);
+};
+
+export const getToken = () => {
+  return sessionStorage.getItem(TOKEN_KEY);
+};
--- frontend/src/org/components/App.js
+++ frontend/src/org/components/App.js
@@ -3,12 +3,10 @@ import { Switch, Route } from "react-router-dom";
 import { Admin, CreateOrg } from "./Router";
 import Dashboard from "../views/Dashboard";
 import Events from "../views/Events";
-import Login from "./SelfSignUp/LogIn";
-import { OrgInfo } from "$modules/SelfSignUp/routes";
+import { OrgInfo, OrgLogo, OrgLinks, Login } from "$modules/SelfSignUp/routes";
 
 import OrgInfoOld from "./SelfSignUp/OrgInfo";
-import OrgLogo from "./SelfSignUp/OrgLogo";
-import OrgLinks from "./SelfSignUp/OrgLinks";
+import OrgLogoOld from "./SelfSignUp/OrgLogo";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "$lib/theme";
 
@@ -24,8 +22,10 @@ const App = () => {
 
         {/* SSU routes */}
         <Route exact path="/org-info/" component={OrgInfo} />
-        <Route exact path="/org-info-old/" component={OrgInfoOld} />
         <Route exact path="/org-logo/" component={OrgLogo} />
+
+        <Route exact path="/org-info-old/" component={OrgInfoOld} />
+        <Route exact path="/org-logo-old/" component={OrgLogoOld} />
         <Route exact path="/org-links/" component={OrgLinks} />
       </Switch>
     </ThemeProvider>
