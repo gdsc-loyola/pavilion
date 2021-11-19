2d152f4284a92f7e3592808f8fce3283329254da
Titia de Castro <decastrotitia@gmail.com>
2021-11-20 02:27:26 +0800

    event card fix

--- frontend/src/components/EventCard.js
+++ frontend/src/components/EventCard.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Card, CardContent, CardMedia, Typography, CardHeader, Avatar } from "@mui/material";
-import { colors,typography } from '../lib/theme'
+import { useHistory } from "react-router"
+import { Card, CardActionArea, CardContent, CardMedia, Typography, CardHeader, Avatar } from "@mui/material";
+import {colors, typography } from '$lib/theme'
 /**
  * @description A MUI TextField with a react-hook-form Controller wrapper
  * @param {Omit<ControllerProps, 'render'> & React.ComponentPropsWithoutRef<typeof TextField>} props
@@ -9,8 +10,12 @@ import { colors,typography } from '../lib/theme'
 
 const EventCard = (props) => {
     const { imgSrc, alt, eventName, startDate, endDate, logoSrc, logoName } = props;
+
+    const history = useHistory()
+
     return (
-        <Card sx={{ maxWidth: 368, boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.08)'}}>
+        <Card sx={{ width: '100%', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.08)'}}>
+        <CardActionArea onClick={() => history.push(`/${logoName.toLowerCase()}/${eventName.toLowerCase()}`)} >
             <CardMedia
                 component="img"
                 height="92"
@@ -18,7 +23,7 @@ const EventCard = (props) => {
                 alt={alt}
             />
             <CardContent sx={{paddingBottom: '0', marginBottom: '16px'}}>
-                <Typography sx={{fontWeight: 600, fontSize: '20px', marginBottom: '8px'}} color={colors.gray[700]} component="p">
+                <Typography sx={{fontWeight: 600, fontSize: '20px', marginBottom: '8px', width: '100%', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'}} color={colors.gray[700]} component="p">
                     {eventName}
                 </Typography>
                 <Typography sx={{fontWeight: typography.fontWeight.reg, fontSize: '16px'}} color={colors.gray[700]} variant="body2">
@@ -39,6 +44,7 @@ const EventCard = (props) => {
                     }
                 }}
             />
+        </CardActionArea>
         </Card>
     )
 }
