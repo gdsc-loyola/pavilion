273ac61e00d2db0a6c1075bf06e19ebf1feb3518
janlaguer <jan.laguer@obf.ateneo.edu>
2022-01-29 23:28:32 +0800

    updated org permission and scope validation

--- orgs/api.py
+++ orgs/api.py
@@ -1,11 +1,15 @@
+import jwt
 from orgs.models import Event, Organization
 from rest_framework import viewsets, permissions
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .permissions import IsGetOrIsAuthenticated, IsPostAndIsAuthenticated, IsPostAndIsNotAuthenticated, IsGet
 from .serializers import UserSerializer, EventsSerializer, OrgsSerializer, UsernameSerializer
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
+from django.http import JsonResponse
+from functools import wraps
 
 # Lead Viewset
 class EventsViewSet(viewsets.ModelViewSet):
@@ -18,7 +22,7 @@ class EventsViewSet(viewsets.ModelViewSet):
 class OrgsViewSet(viewsets.ModelViewSet):
     queryset = Organization.objects.all()
     permission_classes = [  
-        IsGetOrIsAuthenticated
+        IsAuthenticated
     ]
     serializer_class = OrgsSerializer
     lookup_field = 'slug'
@@ -108,3 +112,38 @@ class RegisterViewSet(viewsets.ModelViewSet):
         serializer = UserSerializer(new_user)
 
         return Response(serializer.data)
+
+# Auth0 Scope Validation
+# to use: @requires_scope('scope')
+# example scope 'read:messages' | change permissions in auth0 dashboard
+def get_token_auth_header(request):
+    """
+    Obtains the Access Token from the Authorization Header
+    """
+    auth = request.META.get("HTTP_AUTHORIZATION", None)
+    parts = auth.split()
+    token = parts[1]
+
+    return token
+
+def requires_scope(required_scope):
+    """
+    Determines if the required scope is present in the Access Token
+    Args:
+        required_scope (str): The scope required to access the resource
+    """
+    def require_scope(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            token = get_token_auth_header(args[0])
+            decoded = jwt.decode(token, verify=False)
+            if decoded.get("scope"):
+                token_scopes = decoded["scope"].split()
+                for token_scope in token_scopes:
+                    if token_scope == required_scope:
+                        return f(*args, **kwargs)
+            response = JsonResponse({'message': 'You don\'t have access to this resource'})
+            response.status_code = 403
+            return response
+        return decorated
+    return require_scope
