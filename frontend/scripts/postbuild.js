0ca6c11f146c7b36624ea0f1cded4b1f2a5a19ec
Paolo Tiu <paolotiu17@gmail.com>
2022-02-17 18:47:03 +0800

    use vite as primary build tool

---
+++ frontend/scripts/postbuild.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * Look ma, it's cp -R.
+ * @param {string} src  The path to the thing to copy.
+ * @param {string} dest The path to the new copy.
+ */
+const copyRecursiveSync = (src, dest) => {
+  fs.rmSync(dest, { recursive: true, force: true });
+  var exists = fs.existsSync(src);
+  var stats = exists && fs.statSync(src);
+  var isDirectory = exists && stats.isDirectory();
+  if (isDirectory) {
+    fs.mkdirSync(dest);
+    fs.readdirSync(src).forEach(function (childItemName) {
+      copyRecursiveSync(path.join(src, childItemName), path.join(dest, childItemName));
+    });
+  } else {
+    fs.copyFileSync(src, dest);
+  }
+};
+
+copyRecursiveSync('dist/assets', '../staticfiles/assets');
