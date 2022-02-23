e79037e81cf969a6bd6689a78c6a0ac106f7c301
Charles Lim <charles.andrew.lim@obf.ateneo.edu>
2022-02-23 22:52:47 +0800

    Cleaned up details page

--- frontend/src/modules/EventWizard/Routes/Details.jsx
+++ frontend/src/modules/EventWizard/Routes/Details.jsx
@@ -5,8 +5,9 @@ import TopBar from '../components/TopBar';
 import Layout from '../components/Layout';
 import LinkIcon from '../components/LinkIcon';
 import DownloadIcon from '../components/DownloadIcon';
-import { Button, Box, styled, Typography, Stack, TextField } from '@mui/material';
+import { Button, Box, styled, Typography, Stack, TextField, SvgIcon } from '@mui/material';
 import { colors, typography } from '$lib/theme';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 const Label = styled('label')({
   display: 'flex',
@@ -36,6 +37,7 @@ const HelperText = styled('p')({
   fontSize: typography.fontSize.sm,
   fontWeight: typography.fontWeight.reg,
 });
+
 const coverphoto = null;
 
 const Details = () => {
@@ -129,7 +131,16 @@ const Details = () => {
       </TopBar>
 
       <Box sx={{ marginLeft: '56px', marginTop: '40px', width: '552px' }}>
-        <Typography> Event Details </Typography>
+        <Typography
+          sx={{
+            color: colors.gray[700],
+            fontWeight: typography.fontWeight.bold,
+            fontSize: typography.fontSize.lg,
+            marginBottom: '24px',
+          }}
+        >
+          Event Details
+        </Typography>
 
         <input
           type="file"
@@ -151,6 +162,7 @@ const Details = () => {
             <img
               src={coverphoto ? coverphoto : '../../../../static/assets/image.png'}
               alt="coverPhoto"
+              width="552"
             />
             {!coverphoto && (
               <p>
@@ -209,7 +221,7 @@ const Details = () => {
           size="normal"
           variant="outlined"
           label="Where was the event held?"
-          helperText="0/100"
+          helperText={`${details.location.length}/100`}
           onChange={(e) => {
             handleLocationChange(e.target.value);
           }}
@@ -227,7 +239,7 @@ const Details = () => {
           size="normal"
           variant="outlined"
           label="Describe this event!"
-          helperText="0/100"
+          helperText={`${details.description.length}/100`}
           onChange={(e) => {
             handleDescriptionChange(e.target.value);
           }}
@@ -237,6 +249,214 @@ const Details = () => {
             },
           }}
         />
+      </Box>
+      <Box
+        sx={{
+          marginLeft: '56px',
+          marginRight: '56px',
+          marginTop: '104px',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <Typography
+          sx={{
+            color: colors.gray[700],
+            fontWeight: typography.fontWeight.bold,
+            fontSize: typography.fontSize.lg,
+          }}
+        >
+          Event Photos
+        </Typography>
+        <Typography
+          sx={{
+            marginTop: '8px',
+            color: colors.gray[400],
+            fontSize: typography.fontSize.base,
+            fontWeight: typography.fontWeight.reg,
+          }}
+        >
+          Upload up to 4 images to showcase your event!
+        </Typography>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            gap: '24px',
+            marginTop: '24px',
+            marginBottom: '24px',
+          }}
+        >
+          <input
+            type="file"
+            accept="image/*"
+            id="cover-photo
+        "
+            style={{ display: 'none' }}
+            onChange={(e) => {
+              handleCoverPhotoChange(e.target.files[0]);
+            }}
+          />
+          <Stack sx={{ textAlign: 'center' }} spacing={1}>
+            <Label
+              sx={{ width: '552px', height: '400px' }}
+              htmlFor="cover-photo"
+              onDragOver={(e) => handleDragOver(e)}
+              onDrop={(e) => handleDrop(e)}
+            >
+              <img
+                src={coverphoto ? coverphoto : '../../../../static/assets/image.png'}
+                alt="coverPhoto"
+                width="552"
+              />
+              {!coverphoto && (
+                <p>
+                  <Typography sx={{ color: colors.gray[700] }}>
+                    Drag a cover photo for your event here,
+                  </Typography>
+                  <Typography sx={{ color: colors.blue[400] }}>
+                    <Typography display="inline" sx={{ color: colors.gray[700] }}>
+                      or
+                    </Typography>{' '}
+                    browse through your files.
+                  </Typography>
+                </p>
+              )}
+            </Label>
+            <HelperText>Suggested ratio — 3:2 (ex. 1080x720)</HelperText>
+          </Stack>
+          <input
+            type="file"
+            accept="image/*"
+            id="cover-photo
+        "
+            style={{ display: 'none' }}
+            onChange={(e) => {
+              handleCoverPhotoChange(e.target.files[0]);
+            }}
+          />
+          <Stack sx={{ textAlign: 'center' }} spacing={1}>
+            <Label
+              sx={{ width: '552px', height: '400px' }}
+              htmlFor="cover-photo"
+              onDragOver={(e) => handleDragOver(e)}
+              onDrop={(e) => handleDrop(e)}
+            >
+              <img
+                src={coverphoto ? coverphoto : '../../../../static/assets/image.png'}
+                alt="coverPhoto"
+                width="552"
+              />
+              {!coverphoto && (
+                <p>
+                  <Typography sx={{ color: colors.gray[700] }}>
+                    Drag a cover photo for your event here,
+                  </Typography>
+                  <Typography sx={{ color: colors.blue[400] }}>
+                    <Typography display="inline" sx={{ color: colors.gray[700] }}>
+                      or
+                    </Typography>{' '}
+                    browse through your files.
+                  </Typography>
+                </p>
+              )}
+            </Label>
+            <HelperText>Suggested ratio — 3:2 (ex. 1080x720)</HelperText>
+          </Stack>
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'row', gap: '24px', marginBottom: '135px' }}>
+          <input
+            type="file"
+            accept="image/*"
+            id="cover-photo
+        "
+            style={{ display: 'none' }}
+            onChange={(e) => {
+              handleCoverPhotoChange(e.target.files[0]);
+            }}
+          />
+          <Stack sx={{ textAlign: 'center' }} spacing={1}>
+            <Label
+              sx={{ width: '552px', height: '400px' }}
+              htmlFor="cover-photo"
+              onDragOver={(e) => handleDragOver(e)}
+              onDrop={(e) => handleDrop(e)}
+            >
+              <img
+                src={coverphoto ? coverphoto : '../../../../static/assets/image.png'}
+                alt="coverPhoto"
+                width="552"
+              />
+              {!coverphoto && (
+                <p>
+                  <Typography sx={{ color: colors.gray[700] }}>
+                    Drag a cover photo for your event here,
+                  </Typography>
+                  <Typography sx={{ color: colors.blue[400] }}>
+                    <Typography display="inline" sx={{ color: colors.gray[700] }}>
+                      or
+                    </Typography>{' '}
+                    browse through your files.
+                  </Typography>
+                </p>
+              )}
+            </Label>
+            <HelperText>Suggested ratio — 3:2 (ex. 1080x720)</HelperText>
+          </Stack>
+          <input
+            type="file"
+            accept="image/*"
+            id="cover-photo
+        "
+            style={{ display: 'none' }}
+            onChange={(e) => {
+              handleCoverPhotoChange(e.target.files[0]);
+            }}
+          />
+          <Stack sx={{ textAlign: 'center' }} spacing={1}>
+            <Label
+              sx={{ width: '552px', height: '400px' }}
+              htmlFor="cover-photo"
+              onDragOver={(e) => handleDragOver(e)}
+              onDrop={(e) => handleDrop(e)}
+            >
+              <img
+                src={coverphoto ? coverphoto : '../../../../static/assets/image.png'}
+                alt="coverPhoto"
+                width="552"
+              />
+              {!coverphoto && (
+                <p>
+                  <Typography sx={{ color: colors.gray[700] }}>
+                    Drag a cover photo for your event here,
+                  </Typography>
+                  <Typography sx={{ color: colors.blue[400] }}>
+                    <Typography display="inline" sx={{ color: colors.gray[700] }}>
+                      or
+                    </Typography>{' '}
+                    browse through your files.
+                  </Typography>
+                </p>
+              )}
+            </Label>
+            <HelperText>Suggested ratio — 3:2 (ex. 1080x720)</HelperText>
+          </Stack>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+          height: '72px',
+          border: '1px solid #D1D5DB',
+        }}
+      >
+        <Button size="small" sx={{ marginRight: '56px' }}>
+          Next
+          <SvgIcon fontSize="small" component={KeyboardArrowRightIcon} />
+        </Button>
       </Box>
     </Layout>
   );
