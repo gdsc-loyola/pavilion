4e2168741a33997714a20c2f9bf554664856f860
justin <justinreyes13003@gmail.com>
2023-04-17 23:21:50 +0800

    fix: line length formatting changes

--- orgs/api.py
+++ orgs/api.py
@@ -3,8 +3,20 @@ from orgs.models import Event, Organization, StudentToEvent, Student
 from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .permissions import IsGetOrIsAuthenticated, IsPostAndIsAuthenticated, IsPostAndIsNotAuthenticated, IsGet
-from .serializers import UserSerializer, EventsSerializer, OrgsSerializer, UsernameSerializer, StudentToEventSerializer, StudentSerializer
+from .permissions import (
+    IsGetOrIsAuthenticated,
+    IsPostAndIsAuthenticated,
+    IsPostAndIsNotAuthenticated,
+    IsGet,
+)
+from .serializers import (
+    UserSerializer,
+    EventsSerializer,
+    OrgsSerializer,
+    UsernameSerializer,
+    StudentToEventSerializer,
+    StudentSerializer,
+)
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
@@ -15,45 +27,63 @@ from functools import wraps
 import os
 from mixpanel import Mixpanel
 
-mp = Mixpanel(os.environ['MIXPANEL_API_TOKEN'])
+mp = Mixpanel(os.environ["MIXPANEL_API_TOKEN"])
+
+
 # Lead Viewset
 class EventsViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
-    permission_classes = [
-        IsGetOrIsAuthenticated
-    ]
+    permission_classes = [IsGetOrIsAuthenticated]
     serializer_class = EventsSerializer
 
     def retrieve(self, request, pk, *args, **kwargs):
         current_event = Event.objects.get(pk=pk)
-        query = current_event.student.all() #.student comes from related_name in models.py
+        query = (
+            current_event.student.all()
+        )  # .student comes from related_name in models.py
         if query:
             serializer = StudentToEventSerializer(query, many=True)
             return Response(serializer.data)
-        serializer = EventsSerializer(current_event, context={'request': request})
+        serializer = EventsSerializer(current_event, context={"request": request})
         return Response(serializer.data)
 
     def create(self, request):
         if self.request.user.is_authenticated:
-            
             user = request.user
-            
 
-             # required attributes
-            name = request.data['name']
-            cover_photo = request.data['cover_photo'] if 'cover_photo' in request.data else ''
-            org = Organization.objects.get(user=self.request.user);
-            desc = request.data['desc']
-            location = request.data['location']
-            event_photo1 = request.data['event_photo1'] if 'event_photo1' in request.data else ''
-            event_photo2 = request.data['event_photo2'] if 'event_photo2' in request.data else ''
-            event_photo3 = request.data['event_photo3'] if 'event_photo3' in request.data else ''
-            event_photo4 = request.data['event_photo4'] if 'event_photo4' in request.data else ''
-            start_date = request.data['start_date'] 
-            end_date = request.data['end_date']
-            status = request.data['status']
-            accepting_responses = request.data['accepting_responses'] if 'accepting_responses' in request.data else False
-            is_past_event = request.data['is_past_event'] if 'is_past_event' in request.data else False
+            # required attributes
+            name = request.data["name"]
+            cover_photo = (
+                request.data["cover_photo"] if "cover_photo" in request.data else ""
+            )
+            org = Organization.objects.get(user=self.request.user)
+            desc = request.data["desc"]
+            location = request.data["location"]
+            event_photo1 = (
+                request.data["event_photo1"] if "event_photo1" in request.data else ""
+            )
+            event_photo2 = (
+                request.data["event_photo2"] if "event_photo2" in request.data else ""
+            )
+            event_photo3 = (
+                request.data["event_photo3"] if "event_photo3" in request.data else ""
+            )
+            event_photo4 = (
+                request.data["event_photo4"] if "event_photo4" in request.data else ""
+            )
+            start_date = request.data["start_date"]
+            end_date = request.data["end_date"]
+            status = request.data["status"]
+            accepting_responses = (
+                request.data["accepting_responses"]
+                if "accepting_responses" in request.data
+                else False
+            )
+            is_past_event = (
+                request.data["is_past_event"]
+                if "is_past_event" in request.data
+                else False
+            )
 
             new_event = Event.objects.create(
                 name=name,
@@ -69,41 +99,38 @@ class EventsViewSet(viewsets.ModelViewSet):
                 accepting_responses=accepting_responses,
                 is_past_event=is_past_event,
                 start_date=start_date,
-                end_date=end_date
+                end_date=end_date,
             )
-            new_event.save();
+            new_event.save()
 
-            serializer = EventsSerializer(new_event, context={'request': request})
+            serializer = EventsSerializer(new_event, context={"request": request})
             return Response(serializer.data)
 
         else:
             return Response("Must be authenticated", status=401)
 
 
-
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     permission_classes = [IsGetOrIsAuthenticated]
     serializer_class = StudentSerializer
-    lookup_field = 'id_number'
+    lookup_field = "id_number"
+
 
 class StudentToEventViewSet(viewsets.ModelViewSet):
     queryset = StudentToEvent.objects.all()
-    permission_classes = [
-        IsGetOrIsAuthenticated
-    ]
+    permission_classes = [IsGetOrIsAuthenticated]
     serializer_class = StudentToEventSerializer
+
 
 class OrgsViewSet(viewsets.ModelViewSet):
     queryset = Organization.objects.all()
-    permission_classes = [  
-        IsGetOrIsAuthenticated
-    ]
+    permission_classes = [IsGetOrIsAuthenticated]
     serializer_class = OrgsSerializer
-    lookup_field = 'slug'
+    lookup_field = "slug"
 
     def list(self, *args, **kwargs):
-        #getByOrgUser
+        # getByOrgUser
         if self.request.user.is_authenticated:
             try:
                 query = Organization.objects.get(user=self.request.user)
@@ -111,30 +138,39 @@ class OrgsViewSet(viewsets.ModelViewSet):
                 return Response("Organization not found", status=404)
             except MultipleObjectsReturned:
                 return Response("Multiple Orgs found for current user", status=512)
-            serializer = OrgsSerializer(query, context={'request': self.request})
+            serializer = OrgsSerializer(query, context={"request": self.request})
             return Response(serializer.data)
 
-        #getAll
+        # getAll
         queryset = Organization.objects.all()
-        serializer = [OrgsSerializer(query, context={'request': self.request}).data for query in queryset]
+        serializer = [
+            OrgsSerializer(query, context={"request": self.request}).data
+            for query in queryset
+        ]
         return Response(serializer)
 
     def update(self, request, *args, **kwargs):
-        print(request.data['id'])
-        instance = Organization.objects.get( id = request.data['id'] )
+        print(request.data["id"])
+        instance = Organization.objects.get(id=request.data["id"])
 
         if not instance:
             # return Response(status=status.HTTP_404_NOT_FOUND)
             return Response("Instance not found.", status=404)
-        serializer = self.get_serializer(instance, data=self.request.data, partial=True, context={'request': request})
+        serializer = self.get_serializer(
+            instance, data=self.request.data, partial=True, context={"request": request}
+        )
 
         if not serializer.is_valid():
             return Response("Serializer not valid.", status=401)
         serializer.save()
-        mp.track(serializer.data['name'], "Updated org information", {
-            'old_data': OrgsSerializer(instance, context={'request': request}).data,
-            'new_data': serializer.data
-        })
+        mp.track(
+            serializer.data["name"],
+            "Updated org information",
+            {
+                "old_data": OrgsSerializer(instance, context={"request": request}).data,
+                "new_data": serializer.data,
+            },
+        )
         return Response(serializer.data, status=200)
 
     def create(self, request):
@@ -143,22 +179,22 @@ class OrgsViewSet(viewsets.ModelViewSet):
         data should be FormData in axios
         """
         # required attributes
-        name = request.data['name']
-        short_name = request.data['short_name']
-        slug = request.data['slug']
-        desc = request.data['desc']
-        org_body = request.data['org_body']
+        name = request.data["name"]
+        short_name = request.data["short_name"]
+        slug = request.data["slug"]
+        desc = request.data["desc"]
+        org_body = request.data["org_body"]
         user = request.user
 
         # optional attributes
         # request.FILES=True if there's a file sent and request is sent with headers: { "Content-Type": "multipart/form-data" }
-        logo = request.FILES.get('logo') if request.FILES else ''
-        facebook = request.data['facebook'] if 'facebook' in request.data else ''
-        instagram = request.data['instagram'] if 'instagram' in request.data else ''
-        twitter = request.data['twitter'] if 'twitter' in request.data else ''
-        linkedin = request.data['linkedin'] if 'linkedin' in request.data else ''
-        website = request.data['website'] if 'website' in request.data else ''
-        
+        logo = request.FILES.get("logo") if request.FILES else ""
+        facebook = request.data["facebook"] if "facebook" in request.data else ""
+        instagram = request.data["instagram"] if "instagram" in request.data else ""
+        twitter = request.data["twitter"] if "twitter" in request.data else ""
+        linkedin = request.data["linkedin"] if "linkedin" in request.data else ""
+        website = request.data["website"] if "website" in request.data else ""
+
         new_org = Organization.objects.create(
             name=name,
             short_name=short_name,
@@ -175,14 +211,13 @@ class OrgsViewSet(viewsets.ModelViewSet):
         )
         new_org.save()
 
-        serializer = OrgsSerializer(new_org, context={'request': request})
+        serializer = OrgsSerializer(new_org, context={"request": request})
         return Response(serializer.data)
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    permission_classes = [  
-        IsGet
-    ]
+    permission_classes = [IsGet]
     serializer_class = UsernameSerializer
 
     def retrieve(self, request, pk=None, *args, **kwargs):
@@ -196,13 +231,13 @@ class UserViewSet(viewsets.ModelViewSet):
             serializer = UsernameSerializer(User.objects.get(id=pk)).data
             return Response(serializer)
 
-    #getByUsername
+    # getByUsername
     def list(self, request, *args, **kwargs):
-        userToCheck = request.query_params.get('user', None)
+        userToCheck = request.query_params.get("user", None)
         if userToCheck is not None:
             print(self.request.query_params)
             query = User.objects.get(username=userToCheck)
-            
+
             if not query:
                 return Response("signup")
             return Response(UsernameSerializer(query).data)
@@ -211,12 +246,10 @@ class UserViewSet(viewsets.ModelViewSet):
             serializer = UsernameSerializer(query, many=True)
             return Response(serializer.data)
 
-    
+
 class RegisterViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    permission_classes = [  
-        IsPostAndIsNotAuthenticated
-    ]
+    permission_classes = [IsPostAndIsNotAuthenticated]
     serializer_class = UserSerializer
 
     def create(self, request):
@@ -224,14 +257,14 @@ class RegisterViewSet(viewsets.ModelViewSet):
         password = "9,2Nli1H:C&Vmyl<9:Y)VV1t[jQN7rS7Laf|sip*]X_Fi)IX5"
 
         new_user = User.objects.create(
-            username = username,
-            password = make_password(password)
+            username=username, password=make_password(password)
         )
 
         new_user.save()
         serializer = UserSerializer(new_user)
 
         return Response(serializer.data)
+
 
 # Auth0 Scope Validation
 # to use: @requires_scope('scope')
@@ -246,12 +279,14 @@ def get_token_auth_header(request):
 
     return token
 
+
 def requires_scope(required_scope):
     """
     Determines if the required scope is present in the Access Token
     Args:
         required_scope (str): The scope required to access the resource
     """
+
     def require_scope(f):
         @wraps(f)
         def decorated(*args, **kwargs):
@@ -262,8 +297,12 @@ def requires_scope(required_scope):
                 for token_scope in token_scopes:
                     if token_scope == required_scope:
                         return f(*args, **kwargs)
-            response = JsonResponse({'message': 'You don\'t have access to this resource'})
+            response = JsonResponse(
+                {"message": "You don't have access to this resource"}
+            )
             response.status_code = 403
             return response
+
         return decorated
+
     return require_scope
--- pavilion/urls.py
+++ pavilion/urls.py
@@ -21,25 +21,37 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
+    openapi.Info(
+        title="Snippets API",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
-    path('d-admin/', admin.site.urls),
-    path('', include('orgs.urls')),
-    path('', include('frontend.urls')),
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), 
-    path('api/login/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
-    re_path(r'^api/swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^api/swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^api/redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("d-admin/", admin.site.urls),
+    path("", include("orgs.urls")),
+    path("", include("frontend.urls")),
+    path("api/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/login/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    re_path(
+        r"^api/swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^api/swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^api/redoc/$",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
 ]
